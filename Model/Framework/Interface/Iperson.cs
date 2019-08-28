96527c1f689cb03a6d572ec49a9f459cbd35f304
asus <aminnabavi2@yahoo.com>
2019-08-28 11:40:09 +0430

    Make Being Relation in model

---
+++ Model/Framework/Interface/Iperson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Framework.Interface
+{
+    interface Iperson
+    {
+         string Name { get; set; }
+        string  Surname { get; set; }
+    }
+}
